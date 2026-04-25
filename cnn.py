939a3e50b9e8b09338b4c93fdf9ef20f4ab3bf9b
71yvonne <164125845+71yvonne@users.noreply.github.com>
2026-04-25 11:49:09 +0800

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -11,9 +11,7 @@ class CNN(nn.Module):
     def __init__(self):
         super().__init__()
         self.extract_features = nn.Sequential(
-            nn.Conv2d(
-                1, 32, kernel_size=3, stride=1, padding=1
-            ),  # (batch_size, 32, 28, 28)
+            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),  # (batch_size, 32, 28, 28)
             nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
             nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
@@ -21,9 +19,8 @@ class CNN(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),  # (batch_size, 32, 14, 14)
             nn.Dropout2d(0.15),
-            nn.Conv2d(
-                32, 64, kernel_size=3, stride=1, padding=1
-            ),  # (batch_size, 64, 14, 14)
+            
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),  # (batch_size, 64, 14, 14)
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
@@ -31,9 +28,8 @@ class CNN(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),  # (batch_size, 64, 7, 7)
             nn.Dropout2d(0.30),
-            nn.Conv2d(
-                64, 128, kernel_size=3, stride=1, padding=1
-            ),  # (batch_size, 128, 7, 7)
+            
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),  # (batch_size, 128, 7, 7)
             nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
             nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
@@ -48,10 +44,12 @@ class CNN(nn.Module):
             nn.BatchNorm1d(256),
             nn.ReLU(inplace=True),
             nn.Dropout(0.30),
+            
             nn.Linear(256, 128),
             nn.BatchNorm1d(128),
             nn.ReLU(inplace=True),
             nn.Dropout(0.15),
+            
             nn.Linear(128, 10),
         )
 
